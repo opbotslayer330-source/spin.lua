90f7d572cfa92303b1e3dac53017244ea80e0d9b
opbotslayer330-source <opbotslayer330@gmail.com>
2026-05-09 20:04:37 +0530

    spin.lua
    Beginner

---
+++ spin.lua
@@ -0,0 +1,38 @@
+local Spin = {}
+
+local rewards = {
+    {name = "Common Fruit", chance = 70},
+    {name = "Rare Fruit", chance = 20},
+    {name = "Epic Fruit", chance = 8},
+    {name = "Legendary Fruit", chance = 2}
+}
+
+function Spin.Roll(player)
+    local luck = 1
+
+    if player:FindFirstChild("leaderstats") then
+        local stat = player.leaderstats:FindFirstChild("Luck")
+
+        if stat then
+            luck = stat.Value
+        end
+    end
+
+    local roll = math.random(1, 100)
+    roll = roll / luck
+
+    if roll <= 2 then
+        return rewards[4].name
+
+    elseif roll <= 10 then
+        return rewards[3].name
+
+    elseif roll <= 30 then
+        return rewards[2].name
+
+    else
+        return rewards[1].name
+    end
+end
+
+return Spin
